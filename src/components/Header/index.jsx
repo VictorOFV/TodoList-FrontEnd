e463c1239e94e22919347ec58c9378cb568ba0bf
Victor Manoel Lopes Porto <victormanoellopesporto@gmail.com>
2024-03-01 15:41:38 -0400

    Added a floating menu in the header with some options such as logout, profile and settings.

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,11 +1,55 @@
+import { Link } from "react-router-dom"
 import styles from "./styles.module.scss"
 import logo from "../../assets/icon.svg"
-import { Link } from "react-router-dom"
+import avatar from "../../assets/avatar.jpg"
+import { IconButton, Menu, MenuItem } from "@mui/material"
+import { useContext, useState } from "react"
+import { authContext } from "../../context/Auth"
 
 function Header() {
+    const [open, setOpen] = useState(false)
+    const { logout, user } = useContext(authContext)
+
     return (
         <header className={styles.header}>
-            <Link to={"/"}><img src={logo} alt="logo" /></Link>
+            <Link to={"/"}><img className={styles.logo} src={logo} alt="logo" /></Link>
+            <div className={styles.navContainer}>
+                <nav>
+                    <Link to={"/"}>Minhas Tasks</Link>
+                    <Link to={"/globals"}>Task Globais</Link>
+                </nav>
+                <div>
+                    <IconButton onClick={() => setOpen(true)} >
+                        <img className={styles.avatar} src={avatar} alt="avatar" />
+                    </IconButton>
+                </div>
+            </div>
+
+            <Menu
+                className={styles.menu}
+                id="basic-menu"
+                open={open}
+                onClose={() => setOpen(false)}
+                MenuListProps={{
+                    'aria-labelledby': 'basic-button',
+                }}
+                anchorOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                }}
+                sx={{
+                    marginTop: "2rem",
+                    marginLeft: "-5rem"
+                }}
+            >
+                <MenuItem className={styles.menuItem}>
+                    <Link style={{ color: "#000" }} to={`/profile/${user._id}`}>Perfil</Link>
+                </MenuItem>
+                <MenuItem className={styles.menuItem}>
+                    <Link style={{ color: "#000" }} to={"/settings"}>Configuração</Link>
+                </MenuItem>
+                <MenuItem className={styles.menuItem} onClick={logout}>Sair</MenuItem>
+            </Menu>
         </header>
     )
 }
--- src/hooks/useLogin.js
+++ src/hooks/useLogin.js
@@ -1,6 +1,5 @@
 import { useState } from "react"
 import { useNavigate } from "react-router-dom"
-import { toast } from "react-toastify"
 import api from "../services/apiBackend"
 
 function useLogin() {
